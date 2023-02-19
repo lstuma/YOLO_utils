35a06d8468779cf0f4291ce808ea9d286bac4c0b
sdoxl <g.lstuma@gmail.com>
2023-02-19 18:02:40 +0100

    Dev(setup):  - added project.toml

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # YOLO_utils
 A few utilities for the YOLO project implemented in C for extra speed
+
+## How to use
+```python
+# Import Module
+import yolo_utils
+
+# Declare boxes for IoU Calculation
+box1 = [2, 2, 3, 4]
+box2 = [2, 3, 3, 4]
+
+# Calculate Intersection over Union
+intersec = iou(box1, box2)
+```
--- src/setup.py
+++ src/setup.py
@@ -6,6 +6,6 @@ c_ext = Extension("yolo_utils", sources=["_yolo_utils.c", "yolo_utils.c"])
 setup(
     name='yolo_utils',
     version='0.1',
-    description='This module provides some basic utilities required for training, evaluating and using a YOLO model in C.',
+    description='Extension providing some basic utilities required for training, evaluating and using a YOLO model in C.',
     ext_modules=[c_ext]
 )
